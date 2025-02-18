42fada50dcfaf07e4a3cad9be1d2cd8a83d0d04e
Jeongho Nam <samchon.github@gmail.com>
2025-02-18 14:12:26 +0900

    Feat: `IWrtnAgentRpcListener.initialize()` and test websocket module. (#33)
    Co-authored-by: 오병진 <64676070+sunrabbit123@users.noreply.github.com>

--- test/index.ts
+++ test/index.ts
@@ -1,84 +1,18 @@
 import { DynamicExecutor } from "@nestia/e2e";
-import {
-  HttpLlm,
-  IHttpConnection,
-  IHttpLlmApplication,
-  OpenApi,
-  OpenApiV3,
-  OpenApiV3_1,
-  SwaggerV2,
-} from "@samchon/openapi";
-import ShoppingApi from "@samchon/shopping-api";
-import { WrtnAgent } from "@wrtnlabs/agent";
 import chalk from "chalk";
-import OpenAI from "openai";
-import typia from "typia";
 
 import { TestGlobal } from "./TestGlobal";
 
 const main = async (): Promise<void> => {
   if (!TestGlobal.env.CHATGPT_API_KEY?.length) return;
 
-  // COMPOSE LLM APPLICATION SCHEMA
-  const swagger:
-    | SwaggerV2.IDocument
-    | OpenApiV3.IDocument
-    | OpenApiV3_1.IDocument = await fetch(
-    "https://shopping-be.wrtn.ai/editor/swagger.json",
-  ).then((r) => r.json());
-  const document: OpenApi.IDocument = OpenApi.convert(typia.assert(swagger));
-  const application: IHttpLlmApplication<"chatgpt"> = HttpLlm.application({
-    model: "chatgpt",
-    document,
-  });
-  application.functions = application.functions.filter((f) =>
-    f.path.startsWith("/shoppings/customers"),
-  );
-
-  // HANDSHAKE WITH SHOPPING BACKEND
-  const connection: IHttpConnection = {
-    host: "https://shopping-be.wrtn.ai",
-  };
-  await ShoppingApi.functional.shoppings.customers.authenticate.create(
-    connection,
-    {
-      channel_code: "samchon",
-      external_user: null,
-      href: "htts://127.0.0.1/NodeJS",
-      referrer: null,
-    },
-  );
-  await ShoppingApi.functional.shoppings.customers.authenticate.activate(
-    connection,
-    {
-      mobile: "821012345678",
-      name: "John Doe",
-    },
-  );
-
-  // COMPOSE CHAT AGENT
-  const agent: WrtnAgent = new WrtnAgent({
-    provider: {
-      type: "chatgpt",
-      api: new OpenAI({
-        apiKey: TestGlobal.env.CHATGPT_API_KEY,
-      }),
-      model: "gpt-4o",
-    },
-    controllers: [
-      {
-        protocol: "http",
-        name: "shopping",
-        application,
-        connection,
-      },
-    ],
-  });
-
+  // DO TEST
+  const include: string[] = TestGlobal.getArguments("include");
+  const exclude: string[] = TestGlobal.getArguments("exclude");
   const report: DynamicExecutor.IReport = await DynamicExecutor.validate({
     prefix: "test_",
     location: __dirname + "/features",
-    parameters: () => [agent],
+    parameters: () => [],
     onComplete: (exec) => {
       const trace = (str: string) =>
         console.log(`  - ${chalk.green(exec.name)}: ${str}`);
@@ -89,6 +23,9 @@ const main = async (): Promise<void> => {
         trace(`${chalk.yellow(elapsed.toLocaleString())} ms`);
       } else trace(chalk.red(exec.error.name));
     },
+    filter: (name) =>
+      (include.length ? include.some((str) => name.includes(str)) : true) &&
+      (exclude.length ? exclude.every((str) => !name.includes(str)) : true),
   });
 
   const exceptions: Error[] = report.executions
