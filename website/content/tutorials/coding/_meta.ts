87daeb4a05b2cc482cc98b238fc5f74e040b8544
kakasoo <kscodebase@gmail.com>
2025-03-17 18:11:33 +0900

    docs: add file-system agent by using fs module

--- website/content/tutorials/coding/_meta.ts
+++ website/content/tutorials/coding/_meta.ts
@@ -3,6 +3,7 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   // slack_agent: "💬 Slack",
 
-  github_agent: "📄 Github",
+  github: "🤖 Github",
+  "file-system": "📁 File System",
 };
 export default meta;
