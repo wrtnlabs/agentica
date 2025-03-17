31da3172699408ffaf4a9ff6ebecc02193d72fb4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-17 19:51:04 +0000

    feat(cli): use package list from connectors repo

--- packages/cli/src/utils/getConnectors.ts
+++ packages/cli/src/utils/getConnectors.ts
@@ -1,46 +1,29 @@
-import typia, { tags } from "typia";
+import typia from "typia";
 
-export interface INpmPackages {
-  objects: {
-    package: {
-      name: string;
-    };
-  }[];
-  total: number;
-  time: string & tags.Format<"date-time">;
+export interface Connector {
+  name: string;
+  version: string;
 }
 
+const CONNECTORS_LIST_URL = 'https://raw.githubusercontent.com/wrtnlabs/connectors/refs/heads/feature/list-connectors/connectors-list.json';
+
+export const getConnectorsList = async (): Promise<Connector[]> => {
+  const response = await fetch(CONNECTORS_LIST_URL);
+  const responseJson = (await response.json());
+  const data = typia.assert<Connector[]>(responseJson);
+  return data;
+};
+
 export const getConnectors = async (): Promise<
-  { name: string; value: string }[]
+{ name: string; value: string }[]
 > => {
-  try {
-    const response = await fetch(
-      "https://raw.githubusercontent.com/wrtnlabs/connectors/refs/heads/feature/list-connectors/connectors-list.json",
-    );
-    const responseJson = (await response.json()) as INpmPackages;
-
-    const data = typia.assert<INpmPackages>(responseJson);
-
-    return data.objects
-      .map((pkg) => pkg.package.name)
-      .filter((name: string) => {
-        // shared is not connector package. This is connector util package.
-        if (name === "@wrtnlabs/connector-shared") {
-          return false;
-        }
-
-        const regex = /^@wrtnlabs\/connector-(?:[a-z0-9-]+)+$/;
-        return regex.test(name);
-      })
-      .map((name: string) => {
-        const serviceName = name.replace("@wrtnlabs/connector-", "");
-        return {
-          name: serviceName.replace("-", " ").toUpperCase(),
-          value: serviceName,
-        };
-      });
-  } catch (error) {
-    console.error("Error occurred while fetching package list:", error);
-    return [];
-  }
+  const data = await getConnectorsList();
+  return data
+    .map(({name}) => {
+      const serviceName = name.replace("@wrtnlabs/connector-", "");
+      return {
+        name: serviceName.replace("-", " ").toUpperCase(),
+        value: serviceName,
+      };
+    });
 };
