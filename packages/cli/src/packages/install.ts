c8f4474c381b9218bf618d7afbdcb12c34de08be
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-17 20:24:38 +0000

    test(cli): install command

---
+++ packages/cli/src/packages/install.ts
@@ -0,0 +1,24 @@
+import type { PackageManager } from "./PackageManager";
+
+interface InstallProps {
+  packageManager: PackageManager;
+  pkg: string;
+}
+
+export const install = ({ packageManager, pkg }: InstallProps) => {
+  switch (packageManager) {
+    case "npm":
+      return `npm install ${pkg}`;
+    case "yarn":
+      return `yarn add ${pkg}`;
+    case "pnpm":
+      return `pnpm add ${pkg}`;
+    case "bun":
+      return `bun add ${pkg}`;
+    default:
+      /** exhaustive check */
+      packageManager satisfies never;
+
+      throw new Error(`Unsupported package manager: ${packageManager as unknown as string}`);
+  }
+}
