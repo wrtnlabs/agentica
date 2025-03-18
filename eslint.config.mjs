c74d3bcdb0bed9609751ad4b8b71ebaae61b310f
sunrabbit123 <qudwls185@naver.com>
2025-03-18 14:32:22 +0900

    ci(*): setup eslint draft
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,35 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    ignores: [
+      "node_modules",
+      "packages/*/node_modules",
+      "**/*.js",
+      "**/*.d.ts",
+      "packages/*/lib",
+      "website",
+      "packages/chat",
+    ],
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        project: ["tsconfig.eslint.json", "./packages/*/tsconfig.json"],
+        projectService: {
+          allowDefaultProject: [
+            "*.js",
+            "*.config.ts",
+            "*.config.mjs",
+            "packages/*/*.config.{ts,mjs,cjs}",
+            "packages/*/build/*.mjs",
+            "packages/*/build/*.ts",
+          ],
+        },
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         tsconfigRootDir: import.meta.dirname,
       },
     },
