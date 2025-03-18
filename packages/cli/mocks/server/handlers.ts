ab0b2cd5bd622b3c7ab1913a093c2bd2e2a6bb30
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-18 15:21:03 +0000

    fix(cli): update connector-list

--- packages/cli/mocks/server/handlers.ts
+++ packages/cli/mocks/server/handlers.ts
@@ -1,12 +1,12 @@
 import { http, HttpResponse } from "msw";
 
 export const connectorsListHandler =
-http.get("https://raw.githubusercontent.com/wrtnlabs/connectors/refs/heads/feature/list-connectors/connectors-list.json", () => {
-  const response = [
-    {
-      name: '@wrtnio/connector-google-map',
-      version: '1.0.0',
-    }
-  ]
+http.get("https://raw.githubusercontent.com/wrtnlabs/connectors/refs/heads/main/connectors-list.json", () => {
+  const response = {
+    connectors: [
+      '@wrtnio/connector-google-map',
+    ],
+    version: '1.0.0',
+  }
   return HttpResponse.json(response);
 });
