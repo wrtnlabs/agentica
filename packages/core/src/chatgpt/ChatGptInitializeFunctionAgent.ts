d5fb46bbd392adea424cf9c269851edb7f5cdc5a
sunrabbit123 <qudwls185@naver.com>
2025-03-18 18:10:43 +0900

    feat(core): add text event dispatch on 'initialize' step
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- packages/core/src/chatgpt/ChatGptInitializeFunctionAgent.ts
+++ packages/core/src/chatgpt/ChatGptInitializeFunctionAgent.ts
@@ -4,8 +4,10 @@ import typia from "typia";
 
 import { AgenticaContext } from "../context/AgenticaContext";
 import { __IChatInitialApplication } from "../context/internal/__IChatInitialApplication";
+import { AgenticaTextEvent } from "../events/AgenticaTextEvent";
 import { AgenticaDefaultPrompt } from "../internal/AgenticaDefaultPrompt";
 import { AgenticaSystemPrompt } from "../internal/AgenticaSystemPrompt";
+import { MPSCUtil } from "../internal/MPSCUtil";
 import { StreamUtil } from "../internal/StreamUtil";
 import { AgenticaPrompt } from "../prompts/AgenticaPrompt";
 import { AgenticaTextPrompt } from "../prompts/AgenticaTextPrompt";
@@ -56,8 +58,75 @@ export namespace ChatGptInitializeFunctionAgent {
       parallel_tool_calls: false,
     });
 
-    const chunks = await StreamUtil.readAll(completionStream);
-    const completion = ChatGptCompletionMessageUtil.merge(chunks);
+    const textContext: ({
+      content: string;
+    } & ReturnType<typeof MPSCUtil.create<string>>)[] = [];
+
+    const completion = await StreamUtil.reduce<
+      OpenAI.ChatCompletionChunk,
+      Promise<OpenAI.ChatCompletion>
+    >(completionStream, async (accPromise, chunk) => {
+      const acc = await accPromise;
+      const registerContext = (
+        choices: OpenAI.ChatCompletionChunk.Choice[],
+      ) => {
+        for (const choice of choices) {
+          if (choice.finish_reason) {
+            textContext[choice.index]?.close();
+            continue;
+          }
+          if (!choice.delta.content) {
+            continue;
+          }
+
+          if (textContext[choice.index]) {
+            textContext[choice.index]!.content += choice.delta.content;
+            textContext[choice.index]!.produce(choice.delta.content);
+            continue;
+          }
+
+          const { consumer, produce, close, waitClosed, waitUntilEmpty, done } =
+            MPSCUtil.create<string>();
+
+          textContext[choice.index] = {
+            content: choice.delta.content,
+            consumer,
+            produce,
+            close,
+            waitClosed,
+            waitUntilEmpty,
+            done,
+          };
+          produce(choice.delta.content);
+
+          void ctx.dispatch(
+            new AgenticaTextEvent({
+              role: "assistant",
+              stream: consumer,
+              done,
+              get: () => textContext[choice.index]?.content ?? "",
+              join: async () => {
+                await waitClosed();
+                return textContext[choice.index]!.content;
+              },
+            }),
+          );
+        }
+      };
+
+      if (acc.object === "chat.completion.chunk") {
+        registerContext([acc, chunk].flatMap((acc) => acc.choices));
+        return ChatGptCompletionMessageUtil.merge([acc, chunk]);
+      }
+
+      registerContext(chunk.choices);
+      return ChatGptCompletionMessageUtil.accumulate(acc, chunk);
+    });
+
+    if (!completion) {
+      throw new Error("No completion received");
+    }
+
     //----
     // PROCESS COMPLETION
     //----
@@ -67,7 +136,6 @@ export namespace ChatGptInitializeFunctionAgent {
         choice.message.role === "assistant" &&
         !!choice.message.content?.length
       ) {
-        // @TODO this logic should call the dispatch function
         prompts.push(
           new AgenticaTextPrompt({
             role: "assistant",
