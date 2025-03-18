2e6ee2b95137abba112e3c84c0b5460d66387b9a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-18 15:14:19 +0000

    ci(cli): build with rslib (#142)

---
+++ packages/cli/rslib.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from '@rslib/core';
+import UnpluginTypia from '@ryoppippi/unplugin-typia/rspack';
+
+export default defineConfig({
+  tools: {
+    rspack: {
+      plugins: [
+        UnpluginTypia({ cache: false }),
+      ],
+    },
+  },
+  source: {
+    entry: {
+      index: [
+        './src/index.ts',
+      ],
+    },
+  },
+  lib: [
+    {
+      format: 'cjs',
+      bundle: true,
+      banner: {
+        js: '#!/usr/bin/env node' // Add shebang to the output file
+      },
+      output: {
+        distPath: {
+          root: './bin',
+        },
+        minify: true,
+      },
+    },
+  ],
+});
