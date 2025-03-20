18105e5ff281bf095aadd2c106a67351aa72961c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-20 22:13:01 +0000

    fix(cli): connectors type

--- packages/cli/src/connectors.test.ts
+++ packages/cli/src/connectors.test.ts
@@ -16,9 +16,13 @@ describe('getConnectors', () => {
   it('should return a list of connectors', async () => {
     const connectors = await getConnectors();
     expect(connectors).toEqual([
-      { name: '@WRTNIO/CONNECTOR GOOGLE-MAP', value: '@wrtnio/connector-google-map' }
+      {
+        packageName: '@wrtnio/connector-google-map',
+        serviceName: 'google-map',
+        displayName: 'GOOGLE MAP',
+      },
     ]);
-  });
+});
 })
 
 describe("generateConnectorsArrayCode", () => {
