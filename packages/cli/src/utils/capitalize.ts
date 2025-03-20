7c57dade23d9692e27c10255b43b392d60e72f8e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-20 21:16:10 +0000

    feat(cli): concat utils files into one

--- packages/cli/src/utils/capitalize.ts
+++
@@ -1,7 +0,0 @@
-// Convert first letter to uppercase (ex: aws-s3 -> AwsS3)
-export const capitalize = (service: string): string => {
-  return service
-    .split("-")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join("");
-};
