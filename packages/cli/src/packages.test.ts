5386e8f72d7929ba884653bbc40c0b4eb23feadc
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-20 21:19:34 +0000

    feat(cli): move packages into single file

--- packages/cli/src/packages.test.ts
+++ packages/cli/src/packages.test.ts
@@ -1,33 +1,33 @@
-import { install } from "./install";
+import { installCommand } from "./packages";
 
-describe("install", () => {
+describe("installCommand", () => {
   it('npm', () => {
     const packageManager = "npm";
-    const result = install({ packageManager, pkg: "openai" });
+    const result = installCommand({ packageManager, pkg: "openai" });
     expect(result).toBe("npm install openai");
   });
 
   it('yarn', () => {
     const packageManager = "yarn";
-    const result = install({ packageManager, pkg: "openai" });
+    const result = installCommand({ packageManager, pkg: "openai" });
     expect(result).toBe("yarn add openai");
   });
 
   it('pnpm', () => {
     const packageManager = "pnpm";
-    const result = install({ packageManager, pkg: "openai" });
+    const result = installCommand({ packageManager, pkg: "openai" });
     expect(result).toBe("pnpm add openai");
   });
 
   it('bun', () => {
     const packageManager = "bun";
-    const result = install({ packageManager, pkg: "openai" });
+    const result = installCommand({ packageManager, pkg: "openai" });
     expect(result).toBe("bun add openai");
   });
 
   it('unsupported', () => {
     const packageManager = "unsupported";
     // @ts-expect-error type is not matched
-    expect(() => install({ packageManager, pkg: "openai" })).toThrowError("Unsupported package manager: unsupported");
+    expect(() => installCommand({ packageManager, pkg: "openai" })).toThrowError("Unsupported package manager: unsupported");
   });
 });
