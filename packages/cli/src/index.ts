4028d67e2640e54dd444ea627fff72202437cc35
michael <7471919@naver.com>
2025-03-20 18:08:16 +0900

    fix: add nestjs+react project option

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,7 +2,7 @@ import { Command } from "commander";
 
 import { AgenticaStart } from "./executable/AgenticaStart";
 import { IAgenticaStart } from "./structures/IAgenticaStart";
-import { redBright, blueBright } from './utils/styleText';
+import { blueBright, redBright } from "./utils/styleText";
 
 async function main() {
   const program = new Command();
@@ -11,7 +11,7 @@ async function main() {
     .command("start <directory>")
     .description("Start a new project")
     .option(
-      "-p, --project [nodejs|nestjs|react|standalone]",
+      "-p, --project [nodejs|nestjs|react|nestjs+react|standalone]",
       "The project type",
     )
     .action(async (directory: string, options: IAgenticaStart.IOptions) => {
