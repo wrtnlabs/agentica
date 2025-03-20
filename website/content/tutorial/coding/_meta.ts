9745a51849b6832e1a62e217f9186de85f6fcbb0
Kyungsu Kang <kscodebase@gmail.com>
2025-03-20 14:27:06 +0900

    docs: remove emoji (#166)

--- website/content/tutorial/coding/_meta.ts
+++ website/content/tutorial/coding/_meta.ts
@@ -1,8 +1,8 @@
 import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
-  github: "🤖 Github",
-  "file-system": "📁 File System",
-  swagger: "🔥 Swagger"
+  github: "Github",
+  "file-system": "File System",
+  swagger: "Swagger",
 };
 export default meta;
--- website/content/tutorial/productivity/_meta.ts
+++ website/content/tutorial/productivity/_meta.ts
@@ -1,10 +1,10 @@
 import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
-  gmail: "✉️ Gmail",
-  slack: "🚀 Slack",
-  notion: "📒 Notion",
-  arxiv: "📄 ArXiv",
+  gmail: "Gmail",
+  slack: "Slack",
+  notion: "Notion",
+  arxiv: "ArXiv",
   "google-search": "Google Search",
 };
 export default meta;
