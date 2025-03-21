9f8d7079de51e37a16497600eb3d5fbc665c0def
Amber <81298254+syhgpd@users.noreply.github.com>
2025-03-21 12:58:33 +0900

    Revert "fix(website): Reflect Design QA (#156)"
    This reverts commit 4d8c7dd79526eaa54b1e87c3cb306e54486b5036.

--- website/app/_components/landing/ChatExample.tsx
+++ website/app/_components/landing/ChatExample.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { CHAT_EXAMPLE_MESSAGE_LIST } from "@/app/_constants/landing";
-import { useIntersectionObserver } from "@/app/_hooks/useIntersectionObserver";
+import {
+  CHAT_BUBBLE_DELAY,
+  CHAT_EXAMPLE_MESSAGE_LIST,
+} from "@/app/_constants/landing";
 import { ArrowDown } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
@@ -9,40 +11,23 @@ import { ChatBubble } from "./ChatBubble";
 
 export function ChatExample() {
   const chatContainerRef = useRef<HTMLDivElement>(null);
-  const bottomBoundaryRef = useRef<HTMLDivElement>(null);
-  const [visibleMessage, setVisibleMessage] = useState<number>(0);
-  const [isEnd, setIsEnd] = useState<boolean>(false);
 
-  const bottomBoundaryContent = useIntersectionObserver(bottomBoundaryRef, {
-    rootMargin: "0px",
-  });
+  const [visibleMessages, setVisibleMessages] = useState<number>(0);
 
   useEffect(() => {
-    setIsEnd(!!bottomBoundaryContent?.isIntersecting);
-  }, [bottomBoundaryContent?.isIntersecting]);
-
-  useEffect(() => {
-    if (visibleMessage < CHAT_EXAMPLE_MESSAGE_LIST.length) {
+    if (visibleMessages < CHAT_EXAMPLE_MESSAGE_LIST.length) {
       const delay =
-        CHAT_EXAMPLE_MESSAGE_LIST[visibleMessage].message
-          .split(/\n/)
-          .filter((s) => s.trim() !== "").length *
-          50 +
+        CHAT_EXAMPLE_MESSAGE_LIST[visibleMessages].messages.length *
+          CHAT_BUBBLE_DELAY +
         1000;
 
       const timeout = setTimeout(() => {
-        setVisibleMessage((prev) => prev + 1);
+        setVisibleMessages((prev) => prev + 1);
       }, delay);
 
       return () => clearTimeout(timeout);
     }
-  }, [visibleMessage]);
-
-  const scrollToTop = () => {
-    if (chatContainerRef.current) {
-      chatContainerRef.current?.scrollTo({ top: 0, behavior: "smooth" });
-    }
-  };
+  }, [visibleMessages]);
 
   const scrollToBottom = () => {
     if (chatContainerRef.current) {
@@ -54,29 +39,22 @@ export function ChatExample() {
   };
 
   return (
-    <div className="hidden shrink-0 relative z-10 py-6 px-3 w-[480px] h-[800px] bg-[#27272A]/70 backdrop-blur-[10px] md:block rounded-[20px] ">
+    <div className="bg-[#27272A] shrink-0 relative hidden rounded-[20px] py-6 px-1 z-10 md:block w-[480px] h-[800px]">
       <div
         ref={chatContainerRef}
-        className="h-full px-3 flex flex-col gap-6 overflow-y-scroll [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-zinc-400"
+        className="w-[472px] h-[756px] px-6 overflow-y-scroll md:flex flex-col gap-6 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-zinc-400"
       >
-        {CHAT_EXAMPLE_MESSAGE_LIST.slice(0, visibleMessage).map(
+        {CHAT_EXAMPLE_MESSAGE_LIST.slice(0, visibleMessages).map(
           (message, i) => (
             <ChatBubble key={i} {...message} />
           ),
         )}
-        {visibleMessage === CHAT_EXAMPLE_MESSAGE_LIST.length && (
-          <div ref={bottomBoundaryRef} />
-        )}
       </div>
 
-      {visibleMessage >= 4 && (
+      {visibleMessages >= 4 && (
         <button
-          onClick={isEnd ? scrollToTop : scrollToBottom}
+          onClick={scrollToBottom}
           className="cursor-pointer absolute bottom-4 left-[50%] transform-[translate(-50%,-50%)] bg-zinc-700/70 text-zinc-100 p-2 rounded-full w-fit"
-          style={{
-            transform: isEnd ? "rotate(180deg)" : "rotate(0)",
-            transition: "transform 0.3s",
-          }}
         >
           <ArrowDown size={20} />
         </button>
