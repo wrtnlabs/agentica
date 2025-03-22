463ee852288493b0766791d5838547e3f3dd0673
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-22 22:41:38 +0000

    chore(cli): remove unused codes

--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -7,39 +7,3 @@ export type ProjectOptionValue =
   | "react"
   | "standalone"
   | "nestjs+react";
-
-/**
- * Types about `npx agentica start` command.
- *
- * @author Michael
- */
-export namespace IAgenticaStart {
-  /**
-   * Options for `npx agentica start` command.
-   */
-  export type IOptions = Partial<{
-    project: ProjectOptionValue;
-  }>;
-
-  /**
-   * Parameters for execute `npx agentica start` command.
-   */
-  export interface IExecuteInput {
-    projectName: string;
-    options: IOptions;
-  }
-}
-
-/**
- * Types about options for `npx agentica start` command.
- *
- * @author Michael
- */
-export namespace IAgenticaStartOption {
-  export interface IProject {
-    projectName: string;
-    projectPath: string;
-    openAIKey: string;
-    services: string[];
-  }
-}
