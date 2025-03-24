d17eac1ff14ac4f83899ea472fc850ba9b25b6e4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-24 17:04:09 +0000

    feat(cli): implement auto package manager detection

--- packages/cli/src/packages.test.ts
+++ packages/cli/src/packages.test.ts
@@ -1,4 +1,4 @@
-import { installCommand } from "./packages";
+import { detectPackageManager, installCommand } from "./packages";
 
 describe("installCommand", () => {
   it("npm", () => {
@@ -29,5 +29,41 @@ describe("installCommand", () => {
     const packageManager = "unsupported";
     // @ts-expect-error type is not matched
     expect(() => installCommand({ packageManager, pkg: "openai" })).toThrowError("Unsupported package manager: unsupported");
+  });
+});
+
+describe("detectPackageManager", () => {
+  beforeAll(() => {
+    vi.unstubAllEnvs();
+  });
+
+  it("npm", () => {
+    vi.stubEnv("npm_config_user_agent", "npm/7.24.0 node/v16.13.0 linux x64");
+    const result = detectPackageManager();
+    expect(result).toBe("npm");
+  });
+
+  it("yarn", () => {
+    vi.stubEnv("npm_config_user_agent", "yarn/3.1.1 npm/? node/v16.13.0 linux x64");
+    const result = detectPackageManager();
+    expect(result).toBe("yarn");
+  });
+
+  it("pnpm", () => {
+    vi.stubEnv("npm_config_user_agent", "pnpm/7.24.0 node/v16.13.0 linux x64");
+    const result = detectPackageManager();
+    expect(result).toBe("pnpm");
+  });
+
+  it("bun", () => {
+    vi.stubEnv("npm_config_user_agent", "bun/7.24.0 node/v16.13.0 linux x64");
+    const result = detectPackageManager();
+    expect(result).toBe("bun");
+  });
+
+  it("unsupported", () => {
+    vi.stubEnv("npm_config_user_agent", "unsupported/7.24.0 node/v16.13.0 linux x64");
+    const result = detectPackageManager();
+    expect(result).toBe("npm");
   });
 });
