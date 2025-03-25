48009abb3509666465ae1dec1c2f98217e57d213
Amber <hyeyoenchurros@naver.com>
2025-03-25 16:23:17 +0900

    feat: markdown styling

--- website/app/_components/blog/Markdown.tsx
+++ website/app/_components/blog/Markdown.tsx
@@ -7,5 +7,32 @@ export interface MarkdownProps {
 }
 
 export function Markdown({ children }: MarkdownProps) {
-  return <ReactMarkdown rehypePlugins={[rehypeRaw]}>{children}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      rehypePlugins={[rehypeRaw]}
+      components={{
+        h1: ({ ...props }) => (
+          <h1 {...props} className="text-2xl text-zinc-50 pt-8 pb-4" />
+        ),
+        h2: ({ ...props }) => (
+          <h2 {...props} className="text-xl text-zinc-50 pt-6 pb-3" />
+        ),
+        h3: ({ ...props }) => (
+          <h3 {...props} className="text-lg text-zinc-50 pt-4 pb-2" />
+        ),
+        h4: ({ ...props }) => (
+          <h4 {...props} className="text-base text-zinc-50" />
+        ),
+        h5: ({ ...props }) => (
+          <h5 {...props} className="text-sm text-zinc-50" />
+        ),
+        h6: ({ ...props }) => (
+          <h6 {...props} className="text-xs text-zinc-50" />
+        ),
+        p: ({ ...props }) => <p {...props} className="text-zinc-300" />,
+      }}
+    >
+      {children}
+    </ReactMarkdown>
+  );
 }
