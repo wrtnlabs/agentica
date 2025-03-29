be8fedc3825e9a1c922b98c3342f695e4e2e1425
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-29 17:27:40 +0000

    chore(*): changelogithub

---
+++ changelogithub.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('changelogithub').ChangelogOptions} */
+export default {
+  types: {
+    feat: { title: "🚀 Features" },
+    fix: { title: "🐞 Bug Fixes" },
+    docs: { title: "📝 Documentation" },
+    test: { title: "🧪 Tests" },
+  },
+};
