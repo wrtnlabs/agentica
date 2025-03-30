7cddb70914f678c631641306c111ffa6fbd96a75
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-30 00:30:05 +0000

    fix(website/RemoteSource): definitions order / conditions

--- website/components/RemoteSource.tsx
+++ website/components/RemoteSource.tsx
@@ -2,25 +2,33 @@ import { compileMdx } from "nextra/compile";
 import { MDXRemote } from "nextra/mdx-remote";
 import { VariadicSingleton } from "tstl";
 
-export async function RemoteSource(props: {
+const BRACKET = "```";
+const loader = new VariadicSingleton(async (url: string) => fetch(url).then(async r => r.text()));
+
+interface RemoteSourceProps {
   url: string;
   filename?: string;
   showLineNumbers?: boolean;
   highlight?: string;
-}) {
-  const content: string = await loader.get(props.url);
+}
+
+export async function RemoteSource({
+  url,
+  filename,
+  showLineNumbers,
+  highlight,
+}: RemoteSourceProps) {
+  const content: string = await loader.get(url);
   const header: string = [
     `${BRACKET}typescript`,
-    props.filename?.length
-      ? ` filename=${JSON.stringify(props.filename)}`
+    (filename ?? "").length > 0
+      ? ` filename=${JSON.stringify(filename)}`
       : "",
-    props.showLineNumbers ? " showLineNumbers" : "",
-    props.highlight?.length ? ` {${props.highlight}}` : "",
+    showLineNumbers ?? false ? " showLineNumbers" : "",
+    (highlight ?? "").length > 0 ? ` {${highlight}}` : "",
   ].join("");
   const raw = await compileMdx([header, content.trim(), BRACKET].join("\n"));
   return <MDXRemote compiledSource={raw} />;
 }
-export default RemoteSource;
 
-const BRACKET = "```";
-const loader = new VariadicSingleton(async url => fetch(url).then(async r => r.text()));
+export default RemoteSource;
