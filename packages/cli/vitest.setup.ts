ad1aa9584c22556037efe3db7019f34670cfa9dc
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-31 17:56:59 +0100

    chore(cli): allow to access to `api.github.com`

--- packages/cli/vitest.setup.ts
+++ packages/cli/vitest.setup.ts
@@ -1,5 +1,19 @@
 import { mockServer } from "./mocks/server";
 
-beforeAll(() => mockServer.listen({ onUnhandledRequest: "error" }));
+const ALLOW_REGEX_LIST = [
+  /https:\/\/api\.github\.com\/repos/,
+];
+
+beforeAll(() => mockServer.listen({
+  onUnhandledRequest(req, print) {
+    /* Allow requests */
+    if (ALLOW_REGEX_LIST.some(regex => regex.test(req.url))) {
+      return;
+    }
+
+    /* Deny requests */
+    print.error();
+  },
+}));
 afterEach(() => mockServer.resetHandlers());
 afterAll(() => mockServer.close());
