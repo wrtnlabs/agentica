18e1c6d4f2f48b4137caea1400bfb5c15fef66b2
Amber <hyeyoenchurros@naver.com>
2025-04-03 17:38:03 +0900

    feat: preview card ui

--- website/app/_components/common/preview-card.tsx
+++ website/app/_components/common/preview-card.tsx
@@ -1,6 +1,5 @@
 "use client";
- 
-import { cn } from "@/app/_lib/utils";
+
 import Link from "next/link";
 import { ArrowRightIcon } from "../icons/ArrowRight";
 import Image from "next/image";
@@ -14,7 +13,7 @@ interface PreviewCardProps {
 export function PreviewCard({ title, image, href }: PreviewCardProps) {
   return (
     <Link href={href}>
-      <div className="rounded-s-xl relative z-10 w-full md:w-[420px] md:pr-20 md:h-[370px] overflow-hidden hover:bg-zinc-900 transition-all duration-300">
+      <div className="group rounded-s-xl relative z-10 w-full md:w-[420px] md:pr-20 md:h-[370px] overflow-hidden hover:bg-zinc-900 transition-all duration-300">
         <div className="flex w-full cursor-pointer flex-col gap-3 p-5 pr-10 transition-colors duration-300 md:p-5 md:pr-0 md:hover:bg-transparent">
           <div className="flex items-center justify-between">
             <div className="flex flex-1 flex-col gap-1">
@@ -24,7 +23,7 @@ export function PreviewCard({ title, image, href }: PreviewCardProps) {
             <ArrowRightIcon
               width={48}
               height={48}
-              className={cn("text-zinc-600")}
+              className="text-zinc-600 group-hover:text-zinc-100 transition-all duration-300"
             />
           </div>
           <div className="relative hidden h-[380px] w-[296px] overflow-hidden rounded-[17px] md:block border border-[#86FFD9] drop-shadow-[0_100px_100px_#86FFD966]">
