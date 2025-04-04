c9693feb6371d68b38fcf6264982183c9381d59d
sunrabbit123 <qudwls185@naver.com>
2025-04-04 13:56:38 +0900

    fix
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- packages/pg-vector-selector-benchmark/src/main.ts
+++ packages/pg-vector-selector-benchmark/src/main.ts
@@ -32,18 +32,25 @@ async function main() {
   });
 
   const reports = await Promise.all([
-    runShoppingBenchmark({ agent: plainAgentica }),
-    runShoppingBenchmark({ agent: pgVectorSelectorAgentica }),
+    runShoppingBenchmark({ agent: plainAgentica })
+      .then(v => ({ result: v, name: "plain" })),
+
+    runShoppingBenchmark({ agent: pgVectorSelectorAgentica })
+      .then(v => ({ result: v, name: "pgVectorSelector" })),
   ])
-    .then(reportList => reportList.map(v => v.report()));
+    .then(reportList => reportList.map(v =>
+      ({ name: v.name, result: v.result.report() }),
+    ));
 
   await reports.reduce(async (awiater, docs) => {
     await awiater;
-    const root: string = `./docs/benchmarks/call`;
+    const root: string = `./docs/benchmarks/call/${docs.name}`;
 
-    await rmdir(root);
-    for (const [key, value] of Object.entries(docs)) {
-      await mkdir(path.join(root, key.split("/").slice(0, -1).join("/")));
+    // ignore not exists error
+    await rmdir(root).catch(() => {});
+
+    for (const [key, value] of Object.entries(docs.result)) {
+      await mkdir(path.join(root, key.split("/").slice(0, -1).join("/")), { recursive: true });
       await writeFile(path.join(root, key), value);
     }
   }, Promise.resolve());
