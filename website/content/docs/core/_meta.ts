c5314c3074bf05c8889209c5d7e03ea5d8c1c6d3
Jeongho Nam <samchon.github@gmail.com>
2025-04-11 00:32:43 +0900

    Writing MicroAgentica chapter

--- website/content/docs/core/_meta.ts
+++ website/content/docs/core/_meta.ts
@@ -2,9 +2,10 @@ import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
   index: "Agentic AI",
-  facade: "Facade Controller",
+  facade: "Facade Class",
   config: "Configurations",
   event: "Event Handling",
   history: "Prompt Histories",
+  micro: "Micro Agent",
 };
 export default meta;
