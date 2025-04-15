c81ba644b393941e553d4d9405259926e94a46f6
sunrabbit123 <qudwls185@naver.com>
2025-04-15 21:53:01 +0900

    fix: format
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- packages/pg-vector-selector/src/embed.ts
+++ packages/pg-vector-selector/src/embed.ts
@@ -1,8 +1,11 @@
-import { AgenticaContext } from "@agentica/core/src/context/AgenticaContext";
-import { AgenticaOperation } from "@agentica/core/src/context/AgenticaOperation";
+import type { AgenticaContext } from "@agentica/core/src/context/AgenticaContext";
+import type { AgenticaOperation } from "@agentica/core/src/context/AgenticaOperation";
 import type { ILlmSchema } from "@samchon/openapi";
-import { functional, HttpError, IConnection } from "@wrtnlabs/connector-hive-api";
+import type { IConnection } from "@wrtnlabs/connector-hive-api";
 import type { IApplicationConnectorRetrieval } from "@wrtnlabs/connector-hive-api/lib/structures/connector/IApplicationConnectorRetrieval";
+
+import { functional, HttpError } from "@wrtnlabs/connector-hive-api";
+
 import { getRetry, groupByArray } from "./utils";
 
 export function useEmbeddedContext<SchemaModel extends ILlmSchema.Model>() {
@@ -21,85 +24,87 @@ export function useEmbeddedContext<SchemaModel extends ILlmSchema.Model>() {
   ] as const;
 }
 
-
-
 const retry = getRetry(3);
 
-const embedOperation = (connection: IConnection<object | undefined>) => async <SchemaModel extends ILlmSchema.Model>(
-  controllerName: string,
-  opList: AgenticaOperation<SchemaModel>[],
-) => {
-  const application = await retry(async () =>
-    functional.applications.create(connection, {
-      name: controllerName,
-      description: undefined,
-    }),
-  ).catch(async (e) => {
-    if (!(e instanceof HttpError)) {
-      throw e;
-    }
-    if (e.status !== 409) {
-      throw e;
-    }
+function embedOperation(connection: IConnection<object | undefined>) {
+  return async <SchemaModel extends ILlmSchema.Model>(
+    controllerName: string,
+    opList: AgenticaOperation<SchemaModel>[],
+  ) => {
+    const application = await retry(async () =>
+      functional.applications.create(connection, {
+        name: controllerName,
+        description: undefined,
+      }),
+    ).catch(async (e) => {
+      if (!(e instanceof HttpError)) {
+        throw e;
+      }
+      if (e.status !== 409) {
+        throw e;
+      }
 
-    return retry(async () =>
-      functional.applications.by_names.getByName(
+      return retry(async () =>
+        functional.applications.by_names.getByName(
+          connection,
+          controllerName,
+        ),
+      );
+    });
+
+    const version = await retry(async () =>
+      functional.applications.by_ids.versions.create(
         connection,
-        controllerName,
+        application.id,
+        {},
       ),
     );
-  });
 
-  const version = await retry(async () =>
-    functional.applications.by_ids.versions.create(
-      connection,
-      application.id,
-      {},
-    ),
-  );
-
-  // concurrency request count
-  await groupByArray(opList, 10).reduce(async (accPromise, cur) => {
-    await accPromise;
-    await Promise.all(
-      cur.map(async v =>
-        retry(async () =>
-          functional.application_versions.by_ids.connectors.create(
-            connection,
-            version.id,
-            { name: v.name, description: v.function.description ?? "" },
+    // concurrency request count
+    await groupByArray(opList, 10).reduce(async (accPromise, cur) => {
+      await accPromise;
+      await Promise.all(
+        cur.map(async v =>
+          retry(async () =>
+            functional.application_versions.by_ids.connectors.create(
+              connection,
+              version.id,
+              { name: v.name, description: v.function.description ?? "" },
+            ),
           ),
         ),
+      );
+      return Promise.resolve();
+    }, Promise.resolve());
+
+    return { version, applicationId: application.id };
+  };
+}
+
+export function embedContext(connection: IConnection<object | undefined>) {
+  return async <SchemaModel extends ILlmSchema.Model>(ctx: AgenticaContext<SchemaModel>, setEmbeddedContext: (ctx: AgenticaContext<SchemaModel>, filter: IApplicationConnectorRetrieval.IFilter) => void) => {
+    const filter = await Promise.all(
+      Array.from(ctx.operations.group.entries()).map(
+        async ([key, value]: [
+          string,
+          Map<string, AgenticaOperation<SchemaModel>>,
+        ]) => {
+          const result = await embedOperation(connection)(
+            key,
+            Array.from(value.values()),
+          );
+
+          return {
+            id: result.applicationId,
+            version: result.version.version,
+            type: "byId",
+          } satisfies IApplicationConnectorRetrieval.IFilterApplicationById;
+        },
       ),
     );
-    return Promise.resolve();
-  }, Promise.resolve());
 
-  return { version, applicationId: application.id };
-};
-
-export const embedContext = (connection: IConnection<object | undefined>) => async <SchemaModel extends ILlmSchema.Model>(ctx: AgenticaContext<SchemaModel>, setEmbeddedContext: (ctx: AgenticaContext<SchemaModel>, filter: IApplicationConnectorRetrieval.IFilter) => void) => {
-  const filter = await Promise.all(
-    Array.from(ctx.operations.group.entries()).map(
-      async ([key, value]: [
-        string,
-        Map<string, AgenticaOperation<SchemaModel>>,
-      ]) => {
-        const result = await embedOperation(connection)(
-          key,
-          Array.from(value.values()),
-        );
-
-        return {
-          id: result.applicationId,
-          version: result.version.version,
-          type: "byId",
-        } satisfies IApplicationConnectorRetrieval.IFilterApplicationById;
-      },
-    ),
-  );
-
-  setEmbeddedContext(ctx, {
-    applications: filter,
-  });
-};
+    setEmbeddedContext(ctx, {
+      applications: filter,
+    });
+  };
+}
