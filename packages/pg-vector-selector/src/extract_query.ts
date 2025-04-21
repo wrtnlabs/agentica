8eb74a8115b61168573054d2501bbcfefcff50d7
sunrabbit123 <qudwls185@naver.com>
2025-04-21 16:14:00 +0900

    chore(pg-vector-selector): enhance accurancy
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- packages/pg-vector-selector/src/extract_query.ts
+++ packages/pg-vector-selector/src/extract_query.ts
@@ -5,17 +5,19 @@ import type { FromSchema } from "json-schema-to-ts";
 import { factory, utils } from "@agentica/core";
 
 import { Tools } from "./Tools";
+import { reportStorage } from "./tracking";
 
 export async function extractQuery<SchemaModel extends ILlmSchema.Model>(ctx: AgenticaContext<SchemaModel>) {
   const completionStream = await ctx.request("select", {
     messages: [
       {
-        role: "developer",
+        role: "system",
         content: [
           "You are a function searcher. You will extract search queries from the user's message, and the query results will be function names.",
           "A query is a 2–3 sentence description of the action the user needs to perform.",
           "Therefore, the extracted queries must be suitable for function search.",
           "You need to identify the actions required to achieve what the user wants and extract queries that can be used to search for those actions.",
+          "Extract only one query per task.",
         ].join("\n"),
       },
       ...ctx.histories
@@ -41,5 +43,6 @@ export async function extractQuery<SchemaModel extends ILlmSchema.Model>(ctx: Ag
 
     return arg.query_list.map(v => v.query);
   }) ?? [];
+
   return queries;
 }
