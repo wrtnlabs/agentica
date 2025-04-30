b0b33ae6e05e43dfe594a85d03637de0c2138ec7
sunrabbit123 <qudwls185@naver.com>
2025-04-30 15:31:48 +0900

    feat: modify AgenticaUserInputHistory
    Signed-off-by: sunrabbit123 <qudwls185@naver.com>

--- packages/core/src/histories/AgenticaUserInputHistory.ts
+++ packages/core/src/histories/AgenticaUserInputHistory.ts
@@ -1,10 +1,88 @@
-import type { ChatCompletionContentPart } from "openai/resources/chat/completions/completions";
-
 import type { IAgenticaHistoryJson } from "../json/IAgenticaHistoryJson";
 
 import type { AgenticaHistoryBase } from "./AgenticaHistoryBase";
 
 export interface AgenticaUserInputHistory extends AgenticaHistoryBase<"user_input", IAgenticaHistoryJson.IUserInput> {
   role: "user";
-  contents: Array<ChatCompletionContentPart>;
+  contents: Array<AgenticaUserInputHistory.Contents>;
+}
+
+export namespace AgenticaUserInputHistory {
+  export type Contents = Contents.File | Contents.Image | Contents.InputAudio | Contents.Text;
+  export namespace Contents {
+    interface ContentsBase<Type extends string> {
+      /**
+       * The type of the content part.
+       */
+      type: Type;
+    }
+    /**
+     * Learn about
+     * [text inputs](https://platform.openai.com/docs/guides/text-generation).
+     */
+    export interface Text extends ContentsBase<"text"> {
+      /**
+       * The text content.
+       */
+      text: string;
+    }
+
+    /**
+     * Learn about [image inputs](https://platform.openai.com/docs/guides/vision).
+     */
+    export interface Image extends ContentsBase<"image_url"> {
+      image_url: {
+        /**
+         * Either a URL of the image or the base64 encoded image data.
+         */
+        url: string;
+        /**
+         * Specifies the detail level of the image. Learn more in the
+         * [Vision guide](https://platform.openai.com/docs/guides/vision#low-or-high-fidelity-image-understanding).
+         */
+        detail?: "auto" | "high" | "low";
+      };
+    }
+
+    /**
+     * Learn about [audio inputs](https://platform.openai.com/docs/guides/audio).
+     */
+    export interface InputAudio extends ContentsBase<"input_audio"> {
+      input_audio: {
+        /**
+         * Base64 encoded audio data.
+         */
+        data: string;
+
+        /**
+         * The format of the encoded audio data. Currently supports "wav" and "mp3".
+         */
+        format: "wav" | "mp3";
+      };
+    }
+
+    /**
+     * Learn about [file inputs](https://platform.openai.com/docs/guides/text) for text
+     * generation.
+     */
+    export interface File extends ContentsBase<"file"> {
+      file: {
+        /**
+         * The base64 encoded file data, used when passing the file to the model as a
+         * string.
+         */
+        file_data?: string;
+
+        /**
+         * The ID of an uploaded file to use as input.
+         */
+        file_id?: string;
+
+        /**
+         * The name of the file, used when passing the file to the model as a string.
+         */
+        filename?: string;
+      };
+    }
+  }
 }
