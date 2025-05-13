2378141fd039e5067de214c581a366fe20ed9cd9
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-13 21:16:41 +0100

    fix(website): fetch swagger on build (#433)

--- packages/chat/src/examples/shopping/ShoppingChatApplication.tsx
+++ packages/chat/src/examples/shopping/ShoppingChatApplication.tsx
@@ -10,30 +10,22 @@ import {
   HttpLlm,
   OpenApi,
 } from "@samchon/openapi";
-import { useEffect, useState } from "react";
 import typia from "typia";
 
 import { AgenticaChatApplication } from "../../AgenticaChatApplication";
 
-export function ShoppingChatApplication(props: ShoppingChatApplication.IProps) {
-  const [application, setApplication]
-    = useState<IHttpLlmApplication<ILlmSchema.Model> | null>(null);
-  useEffect(() => {
-    (async () => {
-      setApplication(
-        HttpLlm.application({
-          model: props.schemaModel,
-          document: OpenApi.convert(
-            await fetch(
-              "https://raw.githubusercontent.com/samchon/shopping-backend/refs/heads/master/packages/api/customer.swagger.json",
-            )
-              .then(async r => r.json() as unknown)
-              .then(v => typia.assert<Parameters<typeof OpenApi.convert>[0]>(v)),
-          ),
-        }),
-      );
-    })().catch(console.error);
-  }, []);
+export async function ShoppingChatApplication(props: ShoppingChatApplication.IProps) {
+  const application: IHttpLlmApplication<ILlmSchema.Model> | null = HttpLlm.application({
+    model: props.schemaModel,
+    document: OpenApi.convert(
+      await fetch(
+        "https://raw.githubusercontent.com/samchon/shopping-backend/refs/heads/master/packages/api/customer.swagger.json",
+      )
+        .then(async r => r.json() as unknown)
+        .then(v => typia.assert<Parameters<typeof OpenApi.convert>[0]>(v)),
+    ),
+  });
+
   if (application === null) {
     return (
       <div>
