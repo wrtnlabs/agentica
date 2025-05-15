fca7ee5e34d45b016bcdc4353aeb36187a4c6dc9
Jeongho Nam <samchon.github@gmail.com>
2025-05-15 10:38:56 +0900

    feat(website): polyfill WebAssembly for `pagefind`.

--- website/app/layout.jsx
+++ website/app/layout.jsx
@@ -95,6 +95,9 @@ export default async function RootLayout(props) {
           data-light-mode="true"
           id="guru-widget-id"
         /> */}
+        <Script async type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `self.WebAssembly = self.WebAssembly || (await import("https://unpkg.com/polywasm@0.1.4/index.min.js")).WebAssembly`
+        }} />
       </Head>
       <body>
         <Layout
