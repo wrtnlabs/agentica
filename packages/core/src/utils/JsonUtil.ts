12cb088f82460384493e001072928adc7767431c
Jeongho Nam <samchon.github@gmail.com>
2026-01-01 10:55:46 +0900

    use both of them

--- packages/core/src/utils/JsonUtil.ts
+++ packages/core/src/utils/JsonUtil.ts
@@ -1,14 +1,17 @@
-// import { addMissingBraces, removeEmptyObjectPrefix, removeTrailingCommas } from "es-jsonkit";
+import { addMissingBraces, removeEmptyObjectPrefix, removeTrailingCommas } from "es-jsonkit";
 import { jsonrepair } from "jsonrepair";
 
 export const JsonUtil = {
-  parse<T>(str: string): T {
-    return JSON.parse(jsonrepair(str));
-  },
+  parse,
 };
 
-// const pipe = (...fns: ((str: string) => string)[]) => (str: string) => fns.reduce((acc, fn) => fn(acc), str);
-// function parse(str: string) {
-//   const corrected = pipe(removeEmptyObjectPrefix, addMissingBraces, removeTrailingCommas)(str);
-//   return JSON.parse(corrected);
-// }
+const pipe = (...fns: ((str: string) => string)[]) => (str: string) => fns.reduce((acc, fn) => fn(acc), str);
+function parse(str: string) {
+  const corrected: string = pipe(
+    removeEmptyObjectPrefix,
+    addMissingBraces,
+    removeTrailingCommas,
+    jsonrepair,
+  )(str);
+  return JSON.parse(corrected);
+}
