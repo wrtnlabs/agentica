2340cc4990d35fa4f0d9e3ca55752165b649c779
Jeongho Nam <samchon.github@gmail.com>
2026-01-01 10:51:09 +0900

    feat(agent): `jsonrepair` instead of `es-jsonkit`

--- packages/core/src/utils/JsonUtil.ts
+++ packages/core/src/utils/JsonUtil.ts
@@ -1,11 +1,14 @@
-import { addMissingBraces, removeEmptyObjectPrefix, removeTrailingCommas } from "es-jsonkit";
+// import { addMissingBraces, removeEmptyObjectPrefix, removeTrailingCommas } from "es-jsonkit";
+import { jsonrepair } from "jsonrepair";
 
 export const JsonUtil = {
-  parse,
+  parse<T>(str: string): T {
+    return JSON.parse(jsonrepair(str));
+  },
 };
 
-const pipe = (...fns: ((str: string) => string)[]) => (str: string) => fns.reduce((acc, fn) => fn(acc), str);
-function parse(str: string) {
-  const corrected = pipe(removeEmptyObjectPrefix, addMissingBraces, removeTrailingCommas)(str);
-  return JSON.parse(corrected);
-}
+// const pipe = (...fns: ((str: string) => string)[]) => (str: string) => fns.reduce((acc, fn) => fn(acc), str);
+// function parse(str: string) {
+//   const corrected = pipe(removeEmptyObjectPrefix, addMissingBraces, removeTrailingCommas)(str);
+//   return JSON.parse(corrected);
+// }
